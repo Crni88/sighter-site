f05cd82916b1e6a5d1234de1add03f3e2766e4d2
tarik_kahric <tarik.kahric@edu.fit.ba>
2019-06-14 15:03:48 +0200

    Above and below the fold completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-var message = "Welcome to sighter homepage";
-document.getElementById("message").innerHTML = message;
+var ham = document.getElementById("ham-trigger");
+
+ham.onclick = function() {
+        var navigacija = document.getElementById("menu");
+        navigacija.classList.toggle("open");
+        ham.classList.toggle('fa-times');
+        ham.classList.toggle('fa-bars');
+        var navbar = document.getElementById("navbar");
+        navbar.classList.toggle("black");
+    }
 
